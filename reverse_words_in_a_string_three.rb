4273247a3f8c9ef78d11275f0f6d482cee98f2e5
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-05-29 19:33:38 -0500

    Solved algo

--- reverse_words_in_a_string_three.rb
+++ reverse_words_in_a_string_three.rb
@@ -1,5 +1,12 @@
 def reverse_words(s)
-    
+  # split string into array
+  # iterate through array
+    # reverse each word
+  # combine array back into string
+
+  sentence = s.split(" ")
+
+  p sentence.map { |word| word.reverse! }.join(" ")
 end
 
 reverse_words("Let's take LeetCode contest")
